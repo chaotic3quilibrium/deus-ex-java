594986054988d91afa54c6d3af360a46e67de13f
Jim O'Flaherty <jim.oflaherty.jr@gmail.com>
2025-10-18 12:16:42 -0500

    Add root module-info.java, and make Tuple interface sealed

--- src/main/java/org/deus_ex_java/util/tuple/Tuple.java
+++ src/main/java/org/deus_ex_java/util/tuple/Tuple.java
@@ -3,7 +3,17 @@ package org.deus_ex_java.util.tuple;
 import java.util.stream.Stream;
 
 //TODO: x4 missing javadocs
-public interface Tuple {
+public sealed interface Tuple permits
+    Tuple1,
+    Tuple2,
+    Tuple3,
+    Tuple4,
+    Tuple5,
+    Tuple6,
+    Tuple7,
+    Tuple8,
+    Tuple9,
+    Tuple10 {
   int MIN_ARITY = 1;
   int MAX_ARITY = 10;
 
