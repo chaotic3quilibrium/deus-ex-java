ac12a85571b22056889acc7d52253e1318f1b8f9
Jim O'Flaherty <jim.oflaherty.jr@gmail.com>
2025-12-26 14:51:07 -0600

    Added to StringsOps the String.regionMatches versions of ignore case for indexOf, lastIndexof, and equals to better handle Unicode comparison issues, especially around other languages like the Turkish i

--- src/main/java/org/deus_ex_java/lang/StringsOps.java
+++ src/main/java/org/deus_ex_java/lang/StringsOps.java
@@ -35,13 +35,141 @@ public final class StringsOps {
   }
 
   /**
+   * Returns the index of the first occurrence (left-to-right scan) of the specified substring using the Unicode aware
+   * {@link String#regionMatches(int, String, int, int)}, or 0 if the substring is empty, otherwise -1 if there is no
+   * such occurrence.
+   * <p>
+   * This method avoids using the more common {@link String#toLowerCase} and/or {@link String#toUpperCase} methods, as
+   * both of these methods have been shown to behave inconsistently, or even incorrectly, on languages like Turkish.
+   *
+   * @param source    the string within the left-to-right {@link String#regionMatches(int, String, int, int)} search
+   *                  occurs
+   * @param subString the target string to find
+   * @return the index of the first occurrence (left-to-right scan) of the specified substring using the Unicode aware
+   *     {@link String#regionMatches(int, String, int, int)}, or 0 if the substring is empty, otherwise -1 if there is
+   *     no such occurrence
+   */
+  public static int indexOfIgnoreCase(
+      String source,
+      String subString
+  ) {
+    //caching subString's length
+    var subStringLength = subString.length();
+    if (subStringLength == 0)
+      //empty string is always found by convention at index 0
+      return 0;
+
+    var subStringFirstCharLowerCase = Character.toLowerCase(subString.charAt(0));
+    var subStringFirstCharUpperCase = Character.toUpperCase(subString.charAt(0));
+
+    //stop advancing right when there isn't enough room left for 'subString'
+    for (var i = 0; i <= source.length() - subStringLength; i++) {
+      //quick first character check before calling the more expensive regionMatches() method
+      var sourceCharAtI = source.charAt(i);
+      if ((sourceCharAtI == subStringFirstCharLowerCase) ||
+          (sourceCharAtI == subStringFirstCharUpperCase)
+      ) {
+
+        //language independent match
+        if (source.regionMatches(
+            true,
+            i,
+            subString,
+            0,
+            subStringLength))
+
+          //return the current index 'i' immediately
+          return i;
+      }
+    }
+
+    //no match found is by convention at index -1
+    return -1;
+  }
+
+  /**
+   * Returns the index of the last occurrence (right-to-left scan) of the specified substring using the Unicode aware
+   * {@link String#regionMatches(int, String, int, int)}, or 0 if the substring is empty, otherwise -1 if there is no
+   * such occurrence.
+   * <p>
+   * This method avoids using the more common {@link String#toLowerCase} and/or {@link String#toUpperCase} methods, as
+   * both of these methods have been shown to behave inconsistently, or even incorrectly, on languages like Turkish.
+   *
+   * @param source    the string within which the right-to-left {@link String#regionMatches(int, String, int, int)}
+   *                  search occurs
+   * @param subString the target string to find
+   * @return the index of the last occurrence (right-to-left scan) of the specified substring using the Unicode aware
+   *     {@link String#regionMatches(int, String, int, int)}, or 0 if the substring is empty, otherwise -1 if there is
+   *     no such occurrence
+   */
+  public static int lastIndexOfIgnoreCase(
+      String source,
+      String subString
+  ) {
+    //caching subString's length
+    var subStringLength = subString.length();
+    if (subStringLength == 0)
+      //empty string is always found by convention at index source.length()
+      return source.length();
+
+    var subStringFirstCharLowerCase = Character.toLowerCase(subString.charAt(0));
+    var subStringFirstCharUpperCase = Character.toUpperCase(subString.charAt(0));
+
+    //stop advancing left when there isn't enough room left for 'subString'
+    for (var i = source.length() - subStringLength; i >= 0; i--) {
+      //quick first character check before calling the more expensive regionMatches() method
+      var sourceCharAtI = source.charAt(i);
+      if ((sourceCharAtI == subStringFirstCharLowerCase) ||
+          (sourceCharAtI == subStringFirstCharUpperCase)
+      ) {
+
+        //language independent match
+        if (source.regionMatches(
+            true,
+            i,
+            subString,
+            0,
+            subStringLength))
+
+          //return the current index 'i' immediately
+          return i;
+      }
+    }
+
+    //no match found is by convention at index -1
+    return -1;
+  }
+
+  /**
+   * Returns {@code true} if the two strings are equal in length, and using the Unicode case-insensitive aware function,
+   * {@link #indexOfIgnoreCase(String, String)}, the first occurrence of the second string is at index 0 of the first
+   * string, otherwise {@code false}.
+   * <p>
+   * This method avoids using the more common {@link String#toLowerCase} and/or {@link String#toUpperCase} methods, as
+   * both of these methods have been shown to behave inconsistently, or even incorrectly, on languages like Turkish.
+   *
+   * @param stringA the first string
+   * @param stringB the second string
+   * @return {@code true} if the two strings are equal in length, and using the Unicode case-insensitive aware function,
+   *     {@link #indexOfIgnoreCase(String, String)}, the first occurrence of the second string is at index 0 of the
+   *     first string, otherwise {@code false}
+   */
+  public static boolean equalsIgnoreCase(
+      String stringA,
+      String stringB
+  ) {
+    return (stringA.length() == stringB.length()) &&
+        indexOfIgnoreCase(stringA, stringB) != -1;
+  }
+
+  /**
    * Returns {@code true} if both {@code stringA} and {@code stringB} are {@code null}, or if
-   * {@code stringA.equalsIgnoreCase(stringB)} returns {@code true}, otherwise {@code false}.
+   * {@code equalsIgnoreCase(stringA, stringB)} returns {@code true}, otherwise {@code false}.
    *
    * @param stringA possibly null string
    * @param stringB possibly null string
    * @return {@code true} if both {@code stringA} and {@code stringB} are {@code null}, or if
-   *     {@code stringA.equalsIgnoreCase(stringB)} returns {@code true}, otherwise {@code false}
+   *     {@code equalsIgnoreCase(stringA, stringB)} returns {@code true}, otherwise {@code false}
    */
   public static boolean equalsIgnoreCaseNullable(
       @Nullable String stringA,
@@ -50,6 +178,7 @@ public final class StringsOps {
     var isStringANull = Objects.isNull(stringA);
 
     return (isStringANull == Objects.isNull(stringB)) &&
-        (isStringANull || stringA.equalsIgnoreCase(stringB));
+        (isStringANull || equalsIgnoreCase(stringA, stringB));
   }
+
 }
--- src/test/java/org/deus_ex_java/lang/StringsOpsTests.java
+++ src/test/java/org/deus_ex_java/lang/StringsOpsTests.java
@@ -24,10 +24,49 @@ public class StringsOpsTests {
   }
 
   @Test
+  public void testIndexOfIgnoreCase() {
+    assertEquals(-1, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", "x"));
+    assertEquals(0, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", ""));
+    assertEquals(0, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", "a"));
+    assertEquals(0, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", "A"));
+    assertEquals(3, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", "d"));
+    assertEquals(3, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", "D"));
+    assertEquals(4, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", "eaB"));
+  }
+
+  @Test
+  public void testLastIndexOfIgnoreCase() {
+    assertEquals(-1, StringsOps.lastIndexOfIgnoreCase("AbCdEAbCdE", "x"));
+    assertEquals(10, StringsOps.lastIndexOfIgnoreCase("AbCdEAbCdE", ""));
+    assertEquals(5, StringsOps.lastIndexOfIgnoreCase("AbCdEAbCdE", "a"));
+    assertEquals(5, StringsOps.lastIndexOfIgnoreCase("AbCdEAbCdE", "A"));
+    assertEquals(8, StringsOps.lastIndexOfIgnoreCase("AbCdEAbCdE", "d"));
+    assertEquals(8, StringsOps.lastIndexOfIgnoreCase("AbCdEAbCdE", "D"));
+    assertEquals(4, StringsOps.indexOfIgnoreCase("AbCdEAbCdE", "eaB"));
+  }
+
+  @Test
+  public void testEqualsIgnoreCase() {
+    assertTrue(StringsOps.equalsIgnoreCase("", ""));
+    assertFalse(StringsOps.equalsIgnoreCase("a", ""));
+    assertFalse(StringsOps.equalsIgnoreCase("", "a"));
+    assertTrue(StringsOps.equalsIgnoreCase("a", "a"));
+    assertTrue(StringsOps.equalsIgnoreCase("a", "A"));
+    assertTrue(StringsOps.equalsIgnoreCase("A", "a"));
+    assertTrue(StringsOps.equalsIgnoreCase("A", "A"));
+  }
+
+  @Test
   public void testEqualsIgnoreCaseNullable() {
     assertTrue(StringsOps.equalsIgnoreCaseNullable(null, null));
+    assertFalse(StringsOps.equalsIgnoreCaseNullable("", null));
+    assertFalse(StringsOps.equalsIgnoreCaseNullable(null, ""));
+    assertTrue(StringsOps.equalsIgnoreCaseNullable("", ""));
     assertFalse(StringsOps.equalsIgnoreCaseNullable("a", null));
     assertFalse(StringsOps.equalsIgnoreCaseNullable(null, "A"));
+    assertTrue(StringsOps.equalsIgnoreCaseNullable("a", "a"));
     assertTrue(StringsOps.equalsIgnoreCaseNullable("a", "A"));
+    assertTrue(StringsOps.equalsIgnoreCaseNullable("A", "a"));
+    assertTrue(StringsOps.equalsIgnoreCaseNullable("A", "A"));
   }
 }
