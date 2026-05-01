f7a13f240a670f72f4f2efccdb4e5df36c9ad880
Jim O'Flaherty <jim.oflaherty.jr@gmail.com>
2026-05-01 17:47:41 -0500

    Added parse* options, and related tests

--- src/main/java/org/deus_ex_java/lang/IntegersOps.java
+++ src/main/java/org/deus_ex_java/lang/IntegersOps.java
@@ -1,10 +1,13 @@
 package org.deus_ex_java.lang;
 
 import org.deus_ex_java.util.ArraysOps;
+import org.deus_ex_java.util.Either;
 import org.deus_ex_java.util.SetsOps;
+import org.deus_ex_java.util.TryCatchesOps;
 import org.jspecify.annotations.NullMarked;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -29,5 +32,40 @@ public final class IntegersOps {
     }
 
     return SetsOps.toSetOrdered(Arrays.stream(ArraysOps.findSetBitIndices(bits)).boxed());
+  }
+
+  /**
+   * Return an {@link Optional} containing the {@link Integer} value of the successfully parsed
+   * {@code String possibleInteger}, otherwise {@link Optional#empty()}.
+   * <p>
+   * Convenience function to reify any parsing failure into an {@link Optional#empty()}.
+   *
+   * @param possibleInteger the string value to be parsed
+   * @return an {@link Optional} containing the {@link Integer} value of the successfully parsed
+   * {@code String possibleInteger}, otherwise {@link Optional#empty()}
+   */
+  public static Optional<Integer> parseOptional(String possibleInteger) {
+    return parseEither(possibleInteger).toOptional();
+  }
+
+  /**
+   * Return an {@link Either#right(Object)} containing the {@link Integer} value of the successfully
+   * parsed {@code String possibleInteger}, otherwise {@link Either#left(Object)} containing the
+   * parsing exception.
+   * <p>
+   * Convenience function to reify any parsing failure into an {@link Either#left(Object)}.
+   *
+   * @param possibleInteger the string value to be parsed
+   * @return an {@link Either#right(Object)} containing the {@link Integer} value of the
+   * successfully parsed {@code String possibleInteger}, otherwise {@link Either#left(Object)}
+   * containing the parsing exception
+   */
+  public static Either<NumberFormatException, Integer> parseEither(
+      String possibleInteger
+  ) {
+    return TryCatchesOps.wrap(
+        () ->
+            Integer.parseInt(possibleInteger),
+        NumberFormatException.class);
   }
 }
